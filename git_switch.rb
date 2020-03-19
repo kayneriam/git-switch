dc93887353d0ab62cc7da1d8966b00bd3e137b39
kayneriam <kayner.iam@gmail.com>
2020-03-19 22:36:21 +0300

    Optimizations and new features

--- git_switch.rb
+++ git_switch.rb
@@ -2,8 +2,9 @@
 
 require 'json'
 
-# Git Account Switcher
+# Git Accounts Switcher
 module GitSwitch
+  # Base path to accounts storage: /home/<username>/.accounts/git.json
   DEFAULT_ACCOUNTS_FOLDER = File.join ENV['HOME'], '.accounts'
   DEFAULT_ACCOUNTS_FILE = 'git.json'.freeze
 
@@ -11,17 +12,60 @@ module GitSwitch
     include GitSwitch
   end
 
-  def list
+  def _switch(name, email)
+    `git config --global user.name #{name}`
+    `git config --global user.email #{email}`
+    puts 'Switched to:'
+    current
+  end
+
+  def _find_by_name(name)
+    _accounts.each do |acc|
+      return acc if acc[:name] == name
+    end
+  end
+
+  def _find_by_email(email)
+    _accounts.each do |acc|
+      return acc if acc[:email] == email
+    end
+  end
+
+  def _setup
+    folder_name = DEFAULT_ACCOUNTS_FOLDER
+    path = File.join folder_name, DEFAULT_ACCOUNTS_FILE
+    Dir.mkdir(folder_name) unless Dir.exist?(folder_name)
+    _clear unless File.exist?(path)
+    File.write(File.join(ENV['HOME'], '.bashrc'), "\nalias git-switch='ruby #{Dir.pwd}/#{__FILE__}'", mode: 'a')
+    `source #{File.join ENV['HOME'], '.bashrc'}`
+  end
+
+  def _clear
+    _write({ accounts: [] }.to_json)
+  end
+
+  def _write(arg)
     path = File.join DEFAULT_ACCOUNTS_FOLDER, DEFAULT_ACCOUNTS_FILE
-    JSON.parse(File.read(path), symbolize_names: true)[:accounts].each do |acc|
-      puts "name: #{acc[:name]} \t email: #{acc[:email]}"
+    File.write path, arg
+  end
+
+  def _accounts
+    path = File.join DEFAULT_ACCOUNTS_FOLDER, DEFAULT_ACCOUNTS_FILE
+    JSON.parse(File.read(path), symbolize_names: true)[:accounts]
+  end
+
+  def list
+    accounts = _accounts
+    accounts.each_with_index do |acc, index|
+      puts "name:\t#{acc[:name]}\nemail:\t#{acc[:email]}"
+      puts "" if index != accounts.count - 1
     end
   end
 
   def next
-    path = File.join DEFAULT_ACCOUNTS_FOLDER, DEFAULT_ACCOUNTS_FILE
-    accounts = JSON.parse(File.read(path), symbolize_names: true)[:accounts]
+    accounts = _accounts
     current_name = `git config --global user.name`.chomp
+    switch_trigger = false
     accounts.each_with_index do |account, index|
       next unless account[:name] == current_name
 
@@ -30,16 +74,17 @@ module GitSwitch
       else
         _switch accounts[index + 1][:name], accounts[index + 1][:email]
       end
+
+      switch_trigger = true
       break
     end
-    puts 'Switched to:'
-    current
+    _switch(accounts[0][:name], accounts[0][:email]) unless switch_trigger
   end
 
   def prev
-    path = File.join DEFAULT_ACCOUNTS_FOLDER, DEFAULT_ACCOUNTS_FILE
-    accounts = JSON.parse(File.read(path), symbolize_names: true)[:accounts]
+    accounts = _accounts
     current_name = `git config --global user.name`.chomp
+    switch_trigger = false
     accounts.each_with_index do |account, index|
       next unless account[:name] == current_name
 
@@ -48,10 +93,11 @@ module GitSwitch
       else
         _switch accounts[index - 1][:name], accounts[index - 1][:email]
       end
+
+      switch_trigger = true
       break
     end
-    puts 'Switched to:'
-    current
+    _switch(accounts[0][:name], accounts[0][:email]) unless switch_trigger
   end
 
   def current
@@ -60,10 +106,9 @@ module GitSwitch
   end
 
   def add(name, email)
-    path = File.join DEFAULT_ACCOUNTS_FOLDER, DEFAULT_ACCOUNTS_FILE
-    accounts = JSON.parse(File.read(path), symbolize_names: true)[:accounts]
+    accounts = _accounts
     accounts << { name: name, email: email }
-    File.write path, { accounts: accounts }.to_json
+    _write({ accounts: accounts }.to_json)
   end
 
   def switch_by_name(name)
@@ -74,38 +119,6 @@ module GitSwitch
   def switch_by_email(email)
     account = _find_by_email email
     _switch account[:name], account[:email]
-  end
-
-  def _switch(name, email)
-    `git config --global user.name #{name}`
-    `git config --global user.email #{email}`
-  end
-
-  def _find_by_name(name)
-    path = File.join DEFAULT_ACCOUNTS_FOLDER, DEFAULT_ACCOUNTS_FILE
-    JSON.parse(File.read(path), symbolize_names: true)[:accounts].each do |acc|
-      return acc if acc[:name] == name
-    end
-  end
-
-  def _find_by_email(email)
-    path = File.join DEFAULT_ACCOUNTS_FOLDER, DEFAULT_ACCOUNTS_FILE
-    JSON.parse(File.read(path), symbolize_names: true)[:accounts].each do |acc|
-      return acc if acc[:email] == email
-    end
-  end
-
-  def _setup
-    folder_name = DEFAULT_ACCOUNTS_FOLDER
-    path = File.join folder_name, DEFAULT_ACCOUNTS_FILE
-    Dir.mkdir(folder_name) unless Dir.exist?(folder_name)
-    File.write(path, { accounts: [] }.to_json) unless File.exist?(path)
-    File.write(File.join(ENV['HOME'], '.bashrc'), "\nalias git-switch='ruby #{Dir.pwd}/#{__FILE__}'", mode: 'a')
-  end
-
-  def _clear
-    path = File.join DEFAULT_ACCOUNTS_FOLDER, DEFAULT_ACCOUNTS_FILE
-    File.write(path, { accounts: [] }.to_json)
   end
 end
 
@@ -127,5 +140,9 @@ when 'current'
 when nil
   puts 'Empty command!'
 else
-  puts "Unknown command: #{ARGV[0]}"
+  if ARGV[0].include? '@'
+    GitSwitch.switch_by_email ARGV[0]
+  else
+    GitSwitch.switch_by_name ARGV[0]
+  end
 end
